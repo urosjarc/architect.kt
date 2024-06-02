059bee2e7396bb7e3926e3ae3c61741a77d67c54
urosjarc <jar.fmf@gmail.com>
2024-06-02 11:51:14 +0200

    Start programming on RawDependencyInjectionGenerator

---
+++ lib/src/main/kotlin/com/urosjarc/architect/lib/impl/RawDependencyInjectionGenerator.kt
@@ -0,0 +1,15 @@
+package com.urosjarc.architect.lib.impl
+
+import com.urosjarc.architect.lib.Generator
+import com.urosjarc.architect.lib.domain.AState
+import java.io.File
+
+public class RawDependencyInjectionGenerator(
+    private val appFile: File,
+) : Generator {
+
+
+    override fun generate(aState: AState) {
+    }
+
+}
